35c4de3ca5744544171751c0f436d396421c0a0b
Marcos <m.rodrig.felix@gmail.com>
2021-12-16 23:03:06 -0800

    create Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,42 @@
-import React from "react";
+import { CodeIcon } from '@heroicons/react/solid';
+import React from 'react';
+import { projects } from '../data.js';
+
 
 function Projects() {
   return (
-    <section id="projects">
+    <section id='projects'>
+      <div>
+        <div>
+        <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1>Some of the projects I have built.
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
+            fuga dolore.
+          </p>
+        </div>
+        <div>
+          { 
+            projects.map( (project) => (
+            <a 
+              href={project.title}
+              key={project.description}
+            >
+              <div>
+                <img alt='project' src={project.image}/>
+                <div>
+                  <h2>{project.subtitle}</h2>
+                  <h1>{project.title}</h1>
+                  <p className='leading-relaxed'>{project.description}</p>
+                </div>
+              </div>
+            </a>
+            ))
+          }
+        </div>
+      </div>
     </section>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: 'React Project',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elilaborum quasi incidunt dolore iste nostrum cupiditate voluptas",
+    image: 'IMAGE',
+    subtitle: 'Lorem ipsum dolor',
+    link: 'project/linke/example'
+  },
+  {
+    title: 'React Second Project',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elilaborum quasi incidunt dolore iste nostrum cupiditate voluptas",
+    image: 'IMAGE',
+    subtitle: 'consectetur adipisicing elilaborum',
+    link: 'project/linke/example'
+  },
+];
