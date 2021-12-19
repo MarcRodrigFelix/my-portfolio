a4be92dae9195a7855fd857a3378669cbab4fe60
Marcos <m.rodrig.felix@gmail.com>
2021-12-18 22:24:04 -0800

    add functionality to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,33 @@ import React from "react";
 
 function Contact() {
 
+  const [ name, setName ] = React.useState('');
+  const [ email, setEmail ] = React.useState('');
+  const [ message, setMessage ] = React.useState('');
+
+console.log(name)
+console.log(email)
+console.log(message)
+
+  function encode(data) {
+    return Object.keys(data)
+    .map(
+      (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&")
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message })
+    })
+    .then( () => alert("Message Sent!") )
+    .catch( (error) => alert(error) )
+  }
+
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -28,40 +55,46 @@ function Contact() {
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">EMAIL</h2>
-              <a className="text-indigo-400 leading-relaxed">m.rodrig.felix@gmail.com</a>
+              <a 
+                href="mailto:"
+                className="text-indigo-400 leading-relaxed">m.rodrig.felix@gmail.com</a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">PHONE</h2>
               <p className="leading-relaxed">775-123-5555</p>
             </div>
           </div>
         </div>
         <form 
-          netlify
+          netlify="true"
           name="contact"
+          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">Hire Me</h2>
           <p className="leading-relaxed mb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
             suscipit officia aspernatur veritatis. Asperiores, aliquid</p>
           <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-400">Name</label>
+            <label className="leading-7 text-sm text-gray-400">Name</label>
             <input
               type="text"
               id="name"
               name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+              onChange={ (e) => setName(e.target.value) }
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">Email</label>
+            <label className="leading-7 text-sm text-gray-400">Email</label>
             <input
               type="text"
               id="email"
               name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+              onChange={ (e) => setEmail(e.target.value) }
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
             <label htmlFor="message" className="leading-7 text-sm text-gray-400">Message</label>
             <textarea
               id="message"
               name="message"
+              onChange={ (e) => setMessage(e.target.value) }
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
           </div>
           <button
--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const projects = [
   {
     title: 'React Project',
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elilaborum quasi incidunt dolore iste nostrum cupiditate voluptas",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elilaborum",
     image: 'IMAGE',
     subtitle: 'Lorem ipsum dolor',
     link: 'project/linke/example'
   },
   {
     title: 'React Second Project',
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elilaborum quasi incidunt dolore iste nostrum cupiditate voluptas",
+    description: "quasi incidunt dolore iste nostrum cupiditate voluptas",
     image: 'IMAGE',
     subtitle: 'consectetur adipisicing elilaborum',
     link: 'project/linke/example'
