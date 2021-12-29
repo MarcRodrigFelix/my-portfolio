1c0b56667c683ab48b6053d398008b63e09e9aea
Marcos <m.rodrig.felix@gmail.com>
2021-12-29 01:34:44 -0800

    comment out last 2 projects

--- src/data.js
+++ src/data.js
@@ -1,39 +1,39 @@
 export const projects = [
   {
     title: 'Track My Games',
-    description: 'Built with Ruby, ReactJS, Redux, and Bootstrap for React',
+    description: 'Game tracking app that will help keep track of games a user is currently playing.',
     image: 'IMAGE',
-    subtitle: 'Game tracking app that will help keep track of games a user is currently playing.',
+    subtitle: 'Built with Ruby, ReactJS, Redux, and Bootstrap for React.',
     link: 'https://github.com/MarcRodrigFelix/track-my-games-backend'
   },
   {
     title: 'U Should Play',
-    description: 'Developed with Javascript, Rails API, HTML, CSS.',
+    description: 'SPA(Single Page Application) where users can share games they’ve enjoyed so others can experience them.',
     image: 'IMAGE',
-    subtitle: 'SPA(Single Page Application) where users can share games they’ve enjoyed so others can experience them.',
+    subtitle: 'Developed with Javascript, Rails API, HTML, CSS.',
     link: 'https://github.com/MarcRodrigFelix/u_should_play'
   },
   {
     title: 'Labs \'n Things',
-    description: 'Built with Ruby on Rails, HTML, CSS',
+    description: 'Application to keep track of Dr. appointments and Lab visits.',
     image: 'IMAGE',
-    subtitle: 'Application to keep track of Dr. appointments and Lab visits.',
+    subtitle: 'Built with Ruby on Rails, HTML, CSS',
     link: 'https://github.com/MarcRodrigFelix/labs-and-things'
   },
-  {
-    title: 'Your Favorite Games',
-    description: "iste nostrum cupiditate voluptas",
-    image: 'IMAGE',
-    subtitle: 'consectetur adipisicing elilaborum',
-    link: 'https://github.com/MarcRodrigFelix/your-favorite-games'
-  },
-  {
-    title: 'NBA Teams',
-    description: "Ruby Gem. Can view any NBA team and its roster within terminal",
-    image: 'IMAGE',
-    subtitle: 'consectetur adipisicing elilaborum',
-    link: 'https://github.com/MarcRodrigFelix/NBA_Teams'
-  },
+  // {
+  //   title: 'Your Favorite Games',
+  //   description: 'iste nostrum cupiditate voluptas',
+  //   image: 'IMAGE',
+  //   subtitle: 'consectetur adipisicing elilaborum',
+  //   link: 'https://github.com/MarcRodrigFelix/your-favorite-games'
+  // },
+  // {
+  //   title: 'NBA Teams',
+  //   description: 'Ruby Gem. Can view any NBA team and its roster within terminal',
+  //   image: 'IMAGE',
+  //   subtitle: 'consectetur adipisicing elilaborum',
+  //   link: 'https://github.com/MarcRodrigFelix/NBA_Teams'
+  // },
 ];
 
 
