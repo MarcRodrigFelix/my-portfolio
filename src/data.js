789d84865be44343e3d9e120c4f8390b7724d8bb
Marcos <m.rodrig.felix@gmail.com>
2022-01-11 00:32:59 -0800

    add giphy for NBA teams gem

--- src/data.js
+++ src/data.js
@@ -3,37 +3,37 @@ export const projects = [
     title: 'Track My Games',
     description: 'Game tracking app that will help keep track of games a user is currently playing.',
     image: 'https://media.giphy.com/media/G8EX0ns9BRJkYF1alv/giphy.gif',
-    subtitle: 'Built with Ruby, ReactJS, Redux, and Bootstrap for React.',
+    subtitle: 'Built with ReactJS, Redux, Rails, and Bootstrap',
     link: 'https://github.com/MarcRodrigFelix/track-my-games-backend'
   },
   {
     title: 'U Should Play',
     description: 'SPA(Single Page Application) where users can share games they have enjoyed so others can experience them.',
     image: 'https://media.giphy.com/media/LrGAEPeVEut5N8gqBO/giphy.gif',
-    subtitle: 'Developed with Javascript, Rails API, HTML, CSS.',
+    subtitle: 'Developed with Javascript, Rails API, HTML and CSS.',
     link: 'https://github.com/MarcRodrigFelix/u_should_play'
   },
   {
     title: 'Labs \'n Things',
-    description: 'Application to keep track of Dr. appointments and Lab visits.',
+    description: 'CRUD application to keep track of Dr. appointments and Lab visits.',
     image: 'https://media.giphy.com/media/JDhuE2yE5tkC9mXAzg/giphy.gif',
-    subtitle: 'Built with Ruby on Rails, HTML, CSS',
+    subtitle: 'Built with Ruby on Rails, HTML and CSS.',
     link: 'https://github.com/MarcRodrigFelix/labs-and-things'
   },
   {
     title: 'Your Favorite Games',
-    description: 'iste nostrum cupiditate voluptas',
+    description: 'CRUD application to track your very favorite games.',
     image: 'https://media.giphy.com/media/UKAYqq2aNNnhLQdu7D/giphy.gif',
-    subtitle: 'consectetur adipisicing elilaborum',
+    subtitle: 'Developed with Sinatra, HTML and CSS.',
     link: 'https://github.com/MarcRodrigFelix/your-favorite-games'
   },
-  // {
-  //   title: 'NBA Teams',
-  //   description: 'Ruby Gem. Can view any NBA team and its roster within terminal',
-  //   image: 'IMAGE',
-  //   subtitle: 'consectetur adipisicing elilaborum',
-  //   link: 'https://github.com/MarcRodrigFelix/NBA_Teams'
-  // },
+  {
+    title: 'NBA Teams',
+    description: 'Ruby Gem, cam view any NBA team and its roster within terminal',
+    image: 'https://media.giphy.com/media/xf0s23JXhbqeJjlwUd/giphy.gif',
+    subtitle: 'Created with Ruby',
+    link: 'https://github.com/MarcRodrigFelix/NBA_Teams'
+  },
 ];
 
 
