bffafcf56a7fd3dcbd31776e6135162405888016
Marcos <m.rodrig.felix@gmail.com>
2022-03-03 21:48:51 -0800

    hide ArrowRightIcon import and Contact.js import in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 
 
 function App() {
