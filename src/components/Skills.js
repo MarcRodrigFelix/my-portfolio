0614cf44a7f93bbf70eff7041c61d7136369f4ca
marcos <m.rodrig.felix@gmail.com>
2022-08-11 10:14:45 -0700

    Change sentence in skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ function Skills() {
         <div className="text-center mb-20">
           <ChipIcon className="w-10 inline-block mb-4"/>
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4">Skills &amp; Technologies</h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Some of the languages and frameworks I have learned through my education. Of course, I will continue to add to my knowledge.</p>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Some of the languages and frameworks I have learned through my education so far. Always learning something new!</p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {
